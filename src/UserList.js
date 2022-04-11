f6840785bc5c365797057f0b428e5c848b0c7171
kwaktaemin <kwak9898@github.com>
2022-04-11 17:44:37 +0900

    배열 다루기 수정

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const UserList = () => {
+const UserList = ({ user }) => {
   const users = [
     {
       id: 1,
@@ -21,15 +21,11 @@ const UserList = () => {
 
   return (
     <div>
-      <div>
-        <b>{users[0].username}</b> <span>({users[0].email})</span>
-      </div>
-      <div>
-        <b>{users[1].username}</b> <span>({users[1].email})</span>
-      </div>
-      <div>
-        <b>{users[2].username}</b> <span>({users[2].email})</span>
-      </div>
+      <User user={users[0]} />
+      <User user={users[1]} />
+      <User user={users[2]} />
     </div>
   );
 };
+
+export default UserList;
