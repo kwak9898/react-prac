34eb1f7a783ddd8341db7afeb2e41f06e09ee777
kwaktaemin <kwak9898@github.com>
2022-04-11 18:43:08 +0900

    마운트, 언마운트

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 const User = ({ user, onRemove, onToggle }) => {
+  useEffect(() => {
+    console.log("컴포넌트가 화면에 나타남");
+    return () => {
+      console.log("컴포넌트가 화면에서 사라짐");
+    };
+  }, []);
   return (
     <div>
       <b
@@ -8,7 +14,8 @@ const User = ({ user, onRemove, onToggle }) => {
         onClick={() => onToggle(user.id)}
       >
         {user.username}
-      </b>{" "}
+      </b>
+      &nbsp;
       <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
